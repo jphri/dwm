6a0e27affe02917adf60f7371d6a5315a92920c4
João Pedro Hrihorowitsch Silva <jp.hri.silva@gmail.com>
2025-09-09 19:03:28 -0300

    moved config to file

--- config.c
+++ config.c
@@ -29,116 +29,6 @@ static Arg ploadarg(cfg_t *cfg);
 
 /* See LICENSE file for copyright and license details. */
 
-/* appearance */
-const char col_gray1[]       = "#222222";
-const char col_gray2[]       = "#444444";
-const char col_gray3[]       = "#bbbbbb";
-const char col_gray4[]       = "#eeeeee";
-const char col_cyan[]        = "#005577";
-
-static const char *dmenucmd[] = { "rofi", "-show", "drun", NULL };
-static const char *termcmd[]  = { "st", NULL };
-
-/* key definitions */
-#define MODKEY Mod4Mask
-#define TAGKEYS(KEY,TAG) \
-	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
-
-const Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY|ShiftMask,             XK_r,      restart,        {0} },
-};
-
-const Button buttons[] = {
-	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
-	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
-	{ ClkTagBar,            0,              Button1,        view,           {0} },
-	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
-};
-
-
-Config defaultconfig = {
-	.appearance = {
-		.borderpx  = 1,        /* border pixel of windows */
-		.snap      = 32,       /* snap pixel */
-		.showbar = 1,        /* 0 means no bar */
-		.topbar = 1,        /* 0 means bottom bar */
-		.gappx = 4,
-		.fonts = (const char*[]){ "monospace:size=10" },
-		.fontscount = 1,
-		.colors = {
-			/*               fg         bg         border   */
-			[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-			[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-		}
-	},
-	.lsettings = {
-		.mfact     = 0.55, /* factor of master area size [0.05..0.95] */
-		.nmaster     = 1,    /* number of clients in master area */
-		.resizehints = 1,    /* 1 means respect size hints in tiled resizals */
-		.lockfullscreen = 1, /* 1 will force focus on the fullscreen window */
-		.refreshrate = 120,  /* refresh rate (per second) for client move/resize */
-	},
-	.tags = (char*[]){ "1", "2", "3", "4", "5", "6", "7", "8", "9" },
-	.tagscount = 9,
-
-	.rules = (Rule[]){
-		{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-		{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	},
-	.rulescount = 2,
-
-	.keys = (Key*)keys,
-	.keyscount = LENGTH(keys),
-
-	.buttons = (Button*)buttons,
-	.buttonscount = LENGTH(buttons)
-};
-
 /* layout(s) */
 const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -148,7 +38,7 @@ const Layout layouts[] = {
 };
 int layoutscount = LENGTH(layouts);
 
-Config *currentconfig = &defaultconfig;
+Config *currentconfig = NULL;
 
 void
 loadconfig(const char *path)
@@ -233,7 +123,7 @@ loadconfig(const char *path)
 		return;
 	}
 
-	if(currentconfig != &defaultconfig) {
+	if(currentconfig) {
 		free(currentconfig->appearance.fonts);
 		free(currentconfig->tags);
 		free(currentconfig->rules);
@@ -241,7 +131,6 @@ loadconfig(const char *path)
 		free(currentconfig);
 	}
 	currentconfig = malloc(sizeof(*currentconfig));
-	*currentconfig = defaultconfig;
 
 	loadappearance(cfg_getsec(cfg, "appearance"));
 	loadlayout(cfg_getsec(cfg, "layout"));
